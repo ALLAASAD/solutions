3ad637db01e95cab38f5d4fa5935c3ce8c732788
Shiao Qu <ALLAASAD@protonmail.com>
2021-09-26 11:34:47 +0800

    Create stdc++.h

---
+++ src/include/stdc++.h
@@ -0,0 +1,127 @@
+#pragma once
+
+// Concepts Library
+#include <concepts>
+
+// Coroutines Library
+#include <coroutine>
+
+// Utilities Library
+#include <any>
+#include <bitset>
+#include <cassert>
+#include <cerrno>
+#include <cfloat>
+#include <chrono>
+#include <cinttypes>
+#include <climits>
+#include <compare>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
+#include <ctime>
+#include <exception>
+#include <functional>
+#include <initializer_list>
+#include <limits>
+#include <memory>
+#include <memory_resource>
+#include <new>
+#include <optional>
+#include <scoped_allocator>
+#include <source_location>
+#include <stdexcept>
+#include <system_error>
+#include <tuple>
+#include <type_traits>
+#include <typeindex>
+#include <typeinfo>
+#include <utility>
+#include <variant>
+#include <version>
+
+// Strings Library
+#include <cctype>
+#include <charconv>
+#include <cstring>
+#include <cuchar>
+#include <cwchar>
+#include <cwctype>
+#include <format>
+#include <string>
+#include <string_view>
+
+// Containers Library
+#include <array>
+#include <deque>
+#include <forward_list>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <span>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+// Iterators Library
+#include <iterator>
+
+// Ranges Library
+#include <ranges>
+
+// Algorithms Library
+#include <algorithm>
+#include <execution>
+
+// Numerics Library
+#include <bit>
+#include <cfenv>
+#include <cmath>
+#include <complex>
+#include <numbers>
+#include <numeric>
+#include <random>
+#include <ratio>
+#include <valarray>
+
+// Localization Library
+#include <clocale>
+#include <locale>
+
+// Input/Output Library
+#include <cstdio>
+#include <fstream>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include <sstream>
+#include <streambuf>
+#include <syncstream>
+
+// Filesystem Library
+#include <filesystem>
+
+// Regular Expressions Library
+#include <regex>
+
+// Atomic Operations Library
+#include <atomic>
+
+// Thread Support Library
+#include <barrier>
+#include <condition_variable>
+#include <future>
+#include <latch>
+#include <mutex>
+#include <semaphore>
+#include <shared_mutex>
+#include <stop_token>
+#include <thread>
