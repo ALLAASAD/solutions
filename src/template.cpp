57de676bbde56687b34439e6b4c06d4517422713
Shiao Qu <ALLAASAD@protonmail.com>
2021-09-26 11:34:59 +0800

    Create template.cpp

---
+++ src/template.cpp
@@ -0,0 +1,15 @@
+#ifdef ONLINE_JUDGE
+#include <bits/stdc++.h>
+#else
+#include "../include/stdc++.h"
+#endif
+
+using namespace std;
+
+int main()
+{
+    // ios::sync_with_stdio(false);
+    // cin.tie(nullptr);
+
+    return 0;
+}
